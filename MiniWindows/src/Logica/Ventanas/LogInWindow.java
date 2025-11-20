b9e981c550822ab97a550410d6f1eb7de2df11fd
DJCC <david.carcamo2006@gmail.com>
2025-11-20 17:53:19 -0600

    Desarrollo de logica de usuarios

---
+++ MiniWindows/src/Logica/Ventanas/LogInWindow.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica.Ventanas;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author David
+ */
+public class LogInWindow {
+     public LogInWindow(){
+        JFrame screen = new JFrame();
+        screen.setSize(1080, 720);  //Tamaño standard para menus
+        screen.setResizable(false);
+        screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        screen.setLocationRelativeTo(null);
+        screen.setLayout(null);
+        
+        
+        
+        screen.setVisible(true);
+    }
+    
+    
+    public static void main(String[] args) {
+        LogInWindow ventana = new LogInWindow();
+    }
+    
+}
