818facd9cb45bfca3ee9fb33bb009afe647e10ff
DJCC <david.carcamo2006@gmail.com>
2025-12-07 15:27:01 -0600

    avance todo bugeado pero es un avance

--- MiniWindows/src/Logica/Ventanas/fileExplorer.java
+++ MiniWindows/src/Logica/Ventanas/fileExplorer.java
@@ -4,6 +4,7 @@
  */
 package Logica.Ventanas;
 
+import Logica.Excepciones.nullSelected;
 import Logica.ManejoUsuarios.UserLogged;
 import javax.swing.*;
 import java.awt.BorderLayout;
@@ -46,7 +47,8 @@ import javax.swing.tree.DefaultTreeModel;
  */
 public class fileExplorer extends JPanel{
     private static String userName=UserLogged.getInstance().getUserLogged().getName();
-    private   String raizUsuario =""; //"src\\Z\\Usuarios\\"+userName+"\\"
+    private   String raizUsuario ="";
+    private String setUserRoute = "src\\Z\\Usuarios\\"+userName+"\\";
     private  String recycleBin="";
     private genFondos panelFondo;
     
@@ -76,9 +78,10 @@ public class fileExplorer extends JPanel{
     
     //cosas para portapapeles
     private List<File> clipboardFiles= new ArrayList<>();
+    private List<File> organizerFiles = new ArrayList<>();
     private boolean isCutOperation =false;
     
-    
+    JPanel northPanel = new JPanel(new BorderLayout());
     
     
     public fileExplorer(genFondos panelFondo){
@@ -88,7 +91,7 @@ public class fileExplorer extends JPanel{
         
         //Creacion de rutas dinamicas
          raizUsuario =getRutaCondicionada();
-         recycleBin= raizUsuario+"Papelera";
+         recycleBin= setUserRoute+"Papelera";
         
         
         
@@ -99,30 +102,30 @@ public class fileExplorer extends JPanel{
         setupContentTable();
         
         
-        JPanel northPanel = new JPanel(new BorderLayout());
+        
         
         JToolBar toolBar = setupToolBar();
         northPanel.add(toolBar, BorderLayout.NORTH);
         
         
         
-        //Ordenar
-        JPanel panelSort = new JPanel(new BorderLayout());
-        pathLabel = new JLabel("Ruta Actual: "+ formatDisplayPath(raizUsuario));
-        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
-        panelSort.add(pathLabel, BorderLayout.WEST);
-        
-        setupSortControls(panelSort);
-        northPanel.add(panelSort, BorderLayout.CENTER);
+//        //Ordenar
+//        JPanel panelSort = new JPanel(new BorderLayout());
+//        pathLabel = new JLabel("Ruta Actual: "+ formatDisplayPath(raizUsuario));
+//        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
+//        panelSort.add(pathLabel, BorderLayout.WEST);
+//        
+//        setupSortControls(panelSort);
+//        northPanel.add(panelSort, BorderLayout.CENTER);
         
         
         
         //Panel Principal
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(fileTree), new JScrollPane(fileTable));
         splitPane.setDividerLocation(200);
-        
-        pathLabel = new JLabel("Ruta Actual: "+raizUsuario);
-        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
+//        
+//        pathLabel = new JLabel("Ruta Actual: "+raizUsuario);
+//        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
        // add(pathLabel, BorderLayout.WEST);
         
         //add(panelSort, BorderLayout.NORTH);
@@ -172,6 +175,64 @@ public class fileExplorer extends JPanel{
     }
     
     
+    private JToolBar setupPapeleraBar(){
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+        
+        JButton restaurar = new JButton("<html><b>Restaurar Archivos</b></html>");
+        restaurar.setToolTipText("Restaura los archivos seleccionados");
+        //restaurar..addActionListener(e ->restuararFiles());
+        toolBar.add(restaurar);
+        
+        
+        JButton eliminar = new JButton("<html><b>Eliminar</b></html>");
+        eliminar.setToolTipText("Elimina los archivos permanentemente");
+        //listener aqui
+        toolBar.add(eliminar);
+        
+        
+        //Ordenar
+        JPanel panelSort = new JPanel(new BorderLayout());
+        pathLabel = new JLabel("Ruta Actual: "+ formatDisplayPath(raizUsuario));
+        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
+        panelSort.add(pathLabel, BorderLayout.WEST);
+        
+        setupSortControls(panelSort);
+        northPanel.add(panelSort, BorderLayout.CENTER);
+        
+        pathLabel = new JLabel("Ruta Actual: "+raizUsuario);
+        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
+        
+        
+        
+        return toolBar;
+    }
+    
+    
+    
+    private void actualizarVistaBotones(File carpeta){
+        boolean estaEnPapelera = carpeta.getAbsolutePath().equals(recycleBin);
+        northPanel.removeAll();
+        
+        if(estaEnPapelera){
+            JToolBar BOTONESP= setupPapeleraBar();
+            northPanel.add(BOTONESP, BorderLayout.NORTH);
+            System.out.println("Se ponen los botones de papelera");
+        }else{
+            northPanel.add(setupToolBar(), BorderLayout.NORTH);
+            System.out.println("No estamos en la papelera");
+        }
+        
+        System.out.println(currentDirPath);
+        
+        northPanel.revalidate();
+        northPanel.repaint();
+        
+        
+        
+    }
+    
+    
     private JToolBar setupToolBar(){
         JToolBar toolBar= new JToolBar();
         toolBar.setFloatable(false);
@@ -187,7 +248,19 @@ public class fileExplorer extends JPanel{
         newFileButton.addActionListener(e -> createNewFile());
         toolBar.add(newFileButton);
         
+        
+        JButton importFiles = new JButton("<html><b>Importar Archivos</b></html>");
+        importFiles.addActionListener(e ->ImportFiles());
+        toolBar.add(importFiles);
+        
         //Creacion de opciones de portapapeles
+        
+        JButton organizarBt= new JButton("Organizar");
+         organizarBt.setToolTipText("Organizar archivos en distintas carpetas");
+        organizarBt.addActionListener(e -> organizeFiles());
+        toolBar.add(organizarBt);
+        
+        
         JButton copiarBt= new JButton("Copiar", UIManager.getIcon("Table.copyIcon"));
         copiarBt.setToolTipText("Copia el archivo o carpeta seleccionada");
         copiarBt.addActionListener(e -> copySelectedFiles(false));
@@ -205,12 +278,47 @@ public class fileExplorer extends JPanel{
         eliminarBt.addActionListener(e -> deleteSelectedFiles());
         toolBar.add(eliminarBt);
         
+        JButton renameBt = new JButton("Renombrar");
+        renameBt.setToolTipText("Cambiar el nombre de archivo o carpeta");
+        renameBt.addActionListener(e -> {
+            try{
+                realizarRenombre();
+            }catch(nullSelected nullmsg2){
+                JOptionPane.showMessageDialog(this, nullmsg2.getMessage(), "Atencion", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        toolBar.add(renameBt);
         
         JButton abrirBt = new JButton("Abrir", UIManager.getIcon("InternalFrame.openIcon"));
         abrirBt.setToolTipText("Abrir el archivo seleccionado");
-        abrirBt.addActionListener(e -> openSelectedFile());
+        
+        abrirBt.addActionListener(e ->{
+            
+            //AQUI CASO TRY CATCH CON EXCEPCION PERSONALIZADA
+            try{
+                openSelectedFile();
+            }catch(nullSelected nullmsg){
+                JOptionPane.showMessageDialog(this, nullmsg.getMessage(), "Atencion", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+        });
         toolBar.add(abrirBt);
         
+        
+        //Ordenar
+        JPanel panelSort = new JPanel(new BorderLayout());
+        pathLabel = new JLabel("Ruta Actual: "+ formatDisplayPath(raizUsuario));
+        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
+        panelSort.add(pathLabel, BorderLayout.WEST);
+        
+        setupSortControls(panelSort);
+        northPanel.add(panelSort, BorderLayout.CENTER);
+        
+        
+      
+        
+        pathLabel = new JLabel("Ruta Actual: "+raizUsuario);
+        pathLabel.setBorder(BorderFactory.createEmptyBorder(5,10,5,5));
         
         
         
@@ -218,6 +326,168 @@ public class fileExplorer extends JPanel{
         return toolBar;
     }
     
+    
+    private String obtenerExtension(File archivo){
+        if(archivo.isDirectory()){
+            return "";
+        }
+        
+        String nombre =archivo.getName();
+        int ultimoPunto = nombre.lastIndexOf('.');
+        
+        
+        if(ultimoPunto>0 && ultimoPunto< nombre.length()-1){
+            return nombre.substring(ultimoPunto);
+        }
+        return "";
+        
+    }
+    
+    
+    
+    private void organizeFiles(){
+        JFileChooser folderChooser = new JFileChooser();
+        folderChooser.setDialogTitle("Seleccionar Carpeta de Destino");
+        folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        folderChooser.setMultiSelectionEnabled(true);
+        String rutaUsuarios = "src\\Z\\Usuarios\\"+userName;
+        
+        
+        //ejecutar proceso de copiado
+        
+        copySelectedFilesO();//ver si no tira exception
+        
+        
+        //set de directorio de usuario logeado
+        try{
+            folderChooser.setCurrentDirectory(new File(rutaUsuarios));
+        }catch(Exception ex){
+            
+        }
+        
+        int userSelection = folderChooser.showOpenDialog(this);
+        
+        
+        if(userSelection == JFileChooser.APPROVE_OPTION){
+            File destinationFolder = folderChooser.getSelectedFile();
+            
+            try{
+                String destinationRute = destinationFolder.getCanonicalPath();
+                File dirDestino = new File(destinationRute);
+                pasteFilesO(destinationFolder);
+            }catch(Exception e3){};
+           
+            //displayContents(new File(currentDirPath));
+            
+        }
+    }
+    
+    private boolean renombrar(File archivoOriginal, String nuevoNombre){
+        File dirPadre = archivoOriginal.getParentFile();
+        File nuevoArchivo = new File(dirPadre, nuevoNombre);
+        
+        if(nuevoArchivo.exists()){
+            JOptionPane.showMessageDialog(this, "Ya existe un archivo/directorio con este nombre");
+            return false;
+        }
+        
+        boolean exito = archivoOriginal.renameTo(nuevoArchivo);
+        
+        if(exito){
+            JOptionPane.showMessageDialog(this, "Archivo renombrado con exito");
+        }else{
+            JOptionPane.showMessageDialog(this, "Erro al intentar renombrar el archivo");
+        }
+        return exito;
+    }
+    
+    
+    
+    private void realizarRenombre() throws nullSelected{
+        //para obtener archivo
+        int selectedRow = fileTable.getSelectedRow();
+        
+        //Caso que no se haya seleccionado nada
+        if(selectedRow==-1){
+            throw new nullSelected("No se ha seleccionado ningun archivo");
+        }
+        
+        //verificar que no sea un directorio
+        File  file= (File) fileTable.getValueAt(selectedRow,0);
+        
+        String nameActual = file.getName();
+        String extension = obtenerExtension(file);
+        
+        System.out.println(extension);
+        
+        String nameBase;
+        if(extension.isEmpty()){
+            nameBase =nameActual;
+        }else{
+            nameBase = nameActual.substring(0, nameActual.lastIndexOf('.'));
+        }
+        
+        
+        String newName = JOptionPane.showInputDialog(null, "Ingrese el nuevo nombre: ");
+        
+        if(newName!=null && !newName.trim().isEmpty()){
+            String newNameFinal =newName.trim()+extension;
+            
+            if(!newName.equals(nameActual)){
+                boolean exito = renombrar(file, newNameFinal);
+                if(exito){
+                     displayContents(new File(currentDirPath));
+                }
+            }
+        }else if(newName!= null && newName.equals(nameActual)){
+            
+        }else if(newName!= null){
+            JOptionPane.showMessageDialog(null, "El nuevo nombre no puede estar vacio");
+        }
+        
+        
+        
+    }
+    
+    
+    private void ImportFiles(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("IMPORTAR ARCHIVOS");
+        chooser.setMultiSelectionEnabled(true);
+        
+
+        
+        
+        
+        
+        
+        int result = chooser.showOpenDialog(this);//posiblemente aqui agruegar el screen de pantalla principal
+        
+        
+        if(result == JFileChooser.APPROVE_OPTION){
+            File[] selectedFiles = chooser.getSelectedFiles();
+            File dirDestino = new File(currentDirPath);
+            
+             for(File sourcefile: selectedFiles){
+                try{
+                    
+                    
+                    //AQUI POSIBLEMENTE AGREGAR LA COMPROBACION DE NO PERMITIR ARCHIVOS REPETIDOS
+                    
+                    File destination = new File(dirDestino, sourcefile.getName());
+                    Files.copy(sourcefile.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("Importado: "+sourcefile.getName());
+                    
+                }catch(Exception e){
+                    e.printStackTrace();
+                }   
+            }
+            displayContents(new File(currentDirPath));
+            
+        }
+        
+        
+    }
     
     private void createNewFolder(){
         String folderName = JOptionPane.showInputDialog(this, "Ingresa el nombre de la nueva carpeta:",
@@ -287,10 +557,22 @@ public class fileExplorer extends JPanel{
     
     
     
-    private void openSelectedFile(){
+    
+    
+    
+    private void openSelectedFile() throws nullSelected{
+        
+        
+        //POSIBLEMENTE AQUI EXCEPTION
+        
         int selectedRow = fileTable.getSelectedRow();
+        
+        //Caso que no se haya seleccionado nada
+        if(selectedRow==-1){
+            throw new nullSelected("No se ha seleccionado ningun archivo");
+        }
+        
         //verificar que no sea un directorio
-            
         File  file= (File) fileTable.getValueAt(selectedRow,0);
 
         if(file.isDirectory()){
@@ -349,6 +631,66 @@ public class fileExplorer extends JPanel{
         String operation = isCut ? "Cortar" : "Copiar";
         JOptionPane.showMessageDialog(this, selectedRows.length + "elemento(s) preparado (s) para "+operation +".", "Portapapeles", JOptionPane.INFORMATION_MESSAGE);
     }
+    
+    
+    private void copySelectedFilesO(){
+        int[] selectedRows = fileTable.getSelectedRows();
+        if(selectedRows.length==0){
+            JOptionPane.showMessageDialog(this, "Selecciona los archivos o carpetas a copiar/cortar", "Atencion", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        
+        
+        organizerFiles.clear();
+       
+        
+        for(int row: selectedRows){
+            File file= (File) tableModel.getValueAt(row, 0);
+            organizerFiles.add(file);
+        }
+        JOptionPane.showMessageDialog(this, selectedRows.length + "elemento(s) preparado (s) para organizar .", "Portapapeles", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+    
+    
+    private void pasteFilesO(File destination){
+        
+        if(organizerFiles.isEmpty()){
+            JOptionPane.showMessageDialog(this, "No se han seleccionado archivos para organizar", "Atencion", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+        
+        File targetDir= destination; //modificar esto con el directorio obtenido
+        
+        if(!targetDir.isDirectory()){
+            JOptionPane.showMessageDialog(this,"La ubicacion de destino no es una carpeta valida", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        boolean success = true;
+        List<File> sourceFiles = new ArrayList<>(organizerFiles);
+        
+        for(File sourceFile: sourceFiles){
+            try{
+                File destFile = new File(targetDir, sourceFile.getName());
+                
+                Files.move(sourceFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                
+            }catch(IOException e){
+                success = false;
+                JOptionPane.showMessageDialog(this, "Error al "+(isCutOperation ? "mover" : "copiar")+" "+ sourceFile.getName()+": "+e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+        if(success){
+            organizerFiles.clear();
+            displayContents(targetDir);
+            updateTree(targetDir);
+            JOptionPane.showMessageDialog(this, "Elementos organizados con exito", "Exito", JOptionPane.INFORMATION_MESSAGE);
+        }  
+    }
+    
+    
     
     
     private void pasteFiles(){
@@ -459,6 +801,13 @@ public class fileExplorer extends JPanel{
         }
         
         File papelera = new File(recycleBin);
+        if(!papelera.exists()){
+            papelera.mkdir();
+            File currentDir = new File(currentDirPath);
+            displayContents(currentDir);
+            updateTree(currentDir);
+            
+        }
         boolean success=true;
         
         
@@ -476,7 +825,7 @@ public class fileExplorer extends JPanel{
             
             
             String uniqueName = file.getName()+"-"+ UUID.randomUUID().toString().substring(0,4);
-            File destFile = new File(recycleBin, uniqueName);
+            File destFile = new File(recycleBin, file.getName());
             
             
             try{
@@ -593,6 +942,9 @@ public class fileExplorer extends JPanel{
                 
                 
                 if(file.isDirectory()){
+                    
+                    
+                    actualizarVistaBotones(file);
                     displayContents(file);
                     
                     if(node.getChildCount()==0 || (node.getChildCount() == 1 && node.getFirstChild().toString().equals("Cargando..."))){
@@ -670,7 +1022,7 @@ public class fileExplorer extends JPanel{
                 
                 
                 opcionesOrdenar.setSelectedIndex(col);
-                
+                actualizarVistaBotones(new File(currentDirPath));
                 displayContents(new File(currentDirPath));
             }
             
@@ -682,15 +1034,27 @@ public class fileExplorer extends JPanel{
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e){
                 if(e.getClickCount()==2){
+                    
+                    //QUIZA AQUI EXCEPTION
                     int row = fileTable.getSelectedRow();
                     if(row!=-1){
                         File file = (File) tableModel.getValueAt(fileTable.convertRowIndexToModel(row),0);
                         
                         if(file.isDirectory()){
+                            
+                            
+                            //aqui logica de botones y todo lo demas
+                            
+                            
+                            
+                            
                             displayContents(file);
                             updateTree(file);
                         }
                     }
+                    
+                    
+                    
                 }
             }
         });
@@ -824,6 +1188,8 @@ public class fileExplorer extends JPanel{
         
         @Override
         public Object getValueAt(int rowIndex, int columnIndex) {
+            
+         
             File file = fileList.get(rowIndex);
             
             
